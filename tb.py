854a158e60f2a0a15ab92a2980710043dafbdff4
jilali95 <132459486+jilali95@users.noreply.github.com>
2025-02-25 12:11:59 -0800

    Update tb.py

--- tb.py
+++ tb.py
@@ -11,7 +11,6 @@ users_s = os.getenv("USERS_S", "").strip()
 ALLOWED_USERS = [int(x) for x in users_s.split(',')] if users_s else []
 def load_stock_data():
     file_id = os.getenv("CSV_URL")
-    file_id = str(file_id)
 
     # Set display options to show all rows and all columns
     pd.set_option('display.max_rows', None)
